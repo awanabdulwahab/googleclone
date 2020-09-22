c67304062efcf4dc32d907a2340f90ff63a4f1e7
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-09-22 08:58:49 +0500

    Project is completed

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './SearchPage.css'
+import './SearchPage.css';
 import {useStateValue} from "../StateProvider";
 import useGoogleSearch from "../Keys/useGoogleSearch";
 import Search from "../components/Search";
@@ -14,8 +14,8 @@ import MoreVertIcon from '@material-ui/icons/More'
 function SearchPage() {
     const [{term}, dispatch] = useStateValue();
     // Live API CALL
-    //const {data}  = useGoogleSearch(term);
-    const data = Response;
+    const {data}  = useGoogleSearch(term);
+    // const data = Response;
     // API used
     console.log(data);
     return(
@@ -63,9 +63,31 @@ function SearchPage() {
                     </div>
                 </div>
             </div>
-            <div className={'searchPage_results'}>
-
-            </div>
+            { term &&(
+                <div className={'searchPage_results'}>
+                    <p className='searchPage_resultCount'>
+                        About {data?.searchInformation.formattedTotalResults} results({data?.searchInformation.formattedSearchTime} seconds) for {term}
+                    </p>
+                    {data?.items.map(item =>(
+                    <div className={'searchPage_result'}>
+                        <a className={'searchPage_resultLink'} href={item.link}>
+                            {item.pagemap?.cse_image?.length >0 && item.pagemap?.cse_image[0]?.src && (
+                                <img className={'searchPage_resultImage'} src={item.pagemap?.cse_image[0]?.src}
+                                alt={''}/>
+                                )}
+                            {item.displayLink}
+                        </a>
+                        <a className={'searchPage_resultTitle'} href={item.link}>
+                            <h2>{item.title}</h2>
+                        </a>
+                        <p className={'searchPage_resultSnippet'}>
+                            {item.snippet}
+                        </p>
+                    </div>
+                    )
+                    )}
+                </div>
+            )}
         </div>
     )
 
